46a365bcb5ffb47036bbebcb9c61634233d540cf
Alang Kim <rlaangh77@naver.com>
2022-03-09 01:09:28 +0900

    feat(app): 예시 컴포넌트 작성

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,66 @@
 import { ComponentConstructor } from "../lib/@types/ComponentConstructor.type";
+import { ITodo } from "../lib/@types/types";
 import Component from "../lib/template/Component.template";
 import Counter from "./components/Counter";
+import TodoCounter from "./components/TodoCounter";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 
-class App extends Component<HTMLDivElement> {
-  $$counter: Component;
+class App extends Component<{
+  todos: ITodo[];
+  value?: string;
+}> {
   constructor({ $target }: ComponentConstructor<{}>) {
-    super({ $target });
-    this.$$counter = new Counter({
-      $target: this.$target,
+    super({
+      $target,
+      initialState: { todos: [{ todo: "Hello" }] },
     });
 
-    this.$children = [this.$$counter];
+    this.$children.set("counter", new Counter({ $target }));
+
+    this.$children.set(
+      "input",
+      new TodoInput({
+        $target: this.$target,
+      })
+    );
+
+    this.$children.set(
+      "todoCounter",
+      new TodoCounter({
+        $target: this.$target,
+        initialState: { todos: this.$state.todos },
+      })
+    );
+
+    this.$children.set(
+      "todoList",
+      new TodoList({
+        $target: this.$target,
+        initialState: { todos: this.$state.todos },
+        onCreate: () => {
+          this.setState({
+            todos: [...this.$state.todos, { todo: "Hi Again!" }],
+          });
+        },
+        onToggle: (id) => {
+          const todos = this.$state.todos.map((todo, index) =>
+            index === id ? { ...todo, done: !todo.done } : todo
+          );
+          this.setState({ todos });
+        },
+      })
+    );
+    this.subscribeState({
+      targetState: "todos",
+      subscriber: "todoCounter",
+      subscriberStateKey: "todos",
+    });
+    this.subscribeState({
+      targetState: "todos",
+      subscriber: "todoList",
+      subscriberStateKey: "todos",
+    });
   }
 }
 
